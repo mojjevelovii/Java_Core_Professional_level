2d8227f8eefcc443fc2a3f5e56c70cbfd54da148
Veronika Shumilova <mojjevelovii@gmail.com>
2020-02-25 22:42:50 +0300

    reflection_and_annotations

--- out/production/Java Core_Professional level/reflection_and_annotations/src/main/java/TestClass.java
+++ out/production/Java Core_Professional level/reflection_and_annotations/src/main/java/TestClass.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class TestClass {
+
+    @Test(priority = 2)
+    public static void test1() {
+        System.out.println("test1, priority 2");
+    }
+
+    @Test(priority = 10)
+    public static void test2() {
+        System.out.println("test2, priority 10");
+    }
+
+    @Test(priority = 8)
+    public static void test3() {
+        System.out.println("test3, priority 8");
+    }
+
+    @BeforeSuite()
+    public static void beforeSuite() {
+        System.out.println("BeforeSuite");
+    }
+
+    @AfterSuite()
+    public static void afterSuite() {
+        System.out.println("AfterSuite");
+    }
+
 }
--- out/production/Java Core_Professional level/reflection_and_annotations/src/main/java/TestStarter.java
+++ out/production/Java Core_Professional level/reflection_and_annotations/src/main/java/TestStarter.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class TestStarter {
+    public static void start(Class c) throws Exception {
+        Method[] methods = c.getDeclaredMethods();
+        Comparator<Method> methodComparator;
+        methodComparator = new Comparator<Method>() {
+            int countBefore = 0;
+            int countAfter = 0;
+
+            @Override
+            public int compare(Method o1, Method o2) {
+                int p1 = getPriority(o1);
+                int p2 = getPriority(o2);
+
+                return p1 - p2;
+            }
+
+            private int getPriority(Method method) {
+                if (method.isAnnotationPresent(BeforeSuite.class)) {
+                    if (countBefore > 0) {
+                        throw new RuntimeException("@BeforeSuite должен присутствовать в одном экземпляре.");
+                    }
+                    countBefore++;
+                    return BeforeSuite.priority;
+                } else if (method.isAnnotationPresent(AfterSuite.class)) {
+                    if (countAfter > 0) {
+                        throw new RuntimeException("@AfterSuite должен присутствовать в одном экземпляре.");
+                    }
+                    countAfter++;
+                    return AfterSuite.priority;
+                } else if (method.isAnnotationPresent(Test.class)) {
+                    int priority = method.getAnnotation(Test.class).priority();
+                    if (priority < 1 || priority > 10) {
+                        throw new RuntimeException("Приоритет должен находиться в пределах от 1 до 10.");
+                    }
+                    return priority;
+                }
+                throw new RuntimeException("Аннотация неизвестна.");
+            }
+        };
+        Arrays.sort(methods, methodComparator);
+        for (Method m : methods) {
+            m.invoke(null);
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        start(TestClass.class);
+
+    }
 }
